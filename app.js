33e0f6d6d63b8b3488c110d5c7d928b3f6820cfc
Roger Rodriguez <roger19_07@hotmail.com>
2025-01-21 18:36:37 -0500

    Se crea la función sortearAmigos para elegir aleatoreamente el amigo, se muestra el amigo seleccionado en la etiqueta id:resultado.

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 //Se crea un array para almacenar los nombres indicados por el jugador
 let amigos = [];
 
 //Guarda el nombre escrito en la variable nombreDeAmigo
 function agregarAmigo() {
     let nombreDeAmigo = document.getElementById('amigo').value; 
-
-    console.log(nombreDeAmigo);
 
     //Valida que el campo no este vacio. 
     if (nombreDeAmigo === "") {
@@ -36,4 +33,16 @@ function mostrarAmigos() {
         item.textContent = amigos[i];
         lista.appendChild(item);
     }
+}
+
+//Funcion para escoger el amigo aleatorio, validar que tenga al menos un dato el array
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos en la lista para sortear. Por favor, añade nombres.');
+        return;
+    }
+    const amigoAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[amigoAleatorio];
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>¡El amigo secreto es: <strong>${amigoSorteado}</strong>!</li>`;
 }
