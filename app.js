db7490c6256c4b55ec286c26a1c4e1548fc92af6
Roger Rodriguez <roger19_07@hotmail.com>
2025-01-21 15:57:53 -0500

    actualiza app.js se crea array, validar campo vacio, agrega campo al array.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Se crea un array para almacenar los nombres indicados por el jugador
+let amigos = [];
+
+//Guarda el nombre escrito en la variable nombreDeAmigo
+function agregarAmigo() {
+    let nombreDeAmigo = document.getElementById('amigo').value; 
+
+    console.log(nombreDeAmigo);
+
+    //Valida que el campo no este vacio. 
+    if (nombreDeAmigo === "") {
+        alert ("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nombreDeAmigo); //alimenta el array amigos con los nombres indicados
+    }
+    console.log(amigos);
+}
